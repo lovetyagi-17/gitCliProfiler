480bf158667e03b86268112bc2587e98e0a5da7e
love.tyagi <lovetyagi@smartdatainc.net>
2021-05-08 16:13:36 +0530

    org of userProfile view work.

--- main.js
+++ main.js
@@ -10,13 +10,18 @@ const rl = readline.createInterface({
 rl.question('Enter Your GitHuv Handle name: ', async function (answer)  {
    
     // Api Call
-    const profileData = await axios.get(`https://api.github.com/users/${answer}`);  
-    // console.log(profileData.data);
+    try{
+        var profileData = await axios.get(`https://api.github.com/users/${answer}`);  
+    } catch (error) {
+        console.log(`Something Wrong With ${answer}`);
+    }
 
     const { name, location, followers, following, company } = profileData.data
-    // console.log(name, location, followers, following, company);
-
+    
     const profileTable = new Table();
+    const orgTable = new Table({
+        head: ["Organization", "Description"],
+    });
 
     profileTable.push(
         { Name : name || ""},
@@ -26,8 +31,21 @@ rl.question('Enter Your GitHuv Handle name: ', async function (answer)  {
         { Following : following || ""}
     );
 
-    console.log(profileTable.toString());
 
+    try{
+        var orgData = await axios.get(`https://api.github.com/users/${answer}/orgs`);  
+    } catch (error) {
+        console.log(`Something Wrong With ${answer}`);
+    }
+
+    orgData.data.forEach((org) => {
+        // console.log(org);
+        orgTable.push([org.login, org.description.slice(0, 40) + '.....'])
+    });
+
+     // UserProfile Data
+     console.log(profileTable.toString());
+     console.log(orgTable.toString());
     
     rl.close();
 });
