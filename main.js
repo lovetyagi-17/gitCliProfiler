3b49bce702a9910dd6a2dda4a37d498dc483b5d0
love.tyagi <lovetyagi@smartdatainc.net>
2021-05-08 15:35:16 +0530

    API data in proper format.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { default: axios } = require('axios');
 const readline = require('readline');
+const Table = require('cli-table');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -7,10 +9,25 @@ const rl = readline.createInterface({
 
 rl.question('Enter Your GitHuv Handle name: ', async function (answer)  {
    
-    // api call
-    const profileData = await axios.get(`https://api.github.com/users/${answer}`);
-    
-    console.log(profileData.data);
+    // Api Call
+    const profileData = await axios.get(`https://api.github.com/users/${answer}`);  
+    // console.log(profileData.data);
+
+    const { name, location, followers, following, company } = profileData.data
+    // console.log(name, location, followers, following, company);
+
+    const profileTable = new Table();
+
+    profileTable.push(
+        { Name : name || ""},
+        { Company : company || ""},
+        { Location : location || ""},
+        { Followers : followers || ""},
+        { Following : following || ""}
+    );
+
+    console.log(profileTable.toString());
+
     
     rl.close();
 });
